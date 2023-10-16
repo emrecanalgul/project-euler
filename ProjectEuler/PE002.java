b20e30c1f026e0f78a56792d0b8263e816ba9a6d
Emrecan Algül <can.emre1995@gmail.com>
2023-10-16 17:28:51 +0300

    Project Euler 002 soru çözümü.

---
+++ ProjectEuler/PE002.java
@@ -0,0 +1,24 @@
+package ProjectEuler;
+
+import base.IProjectEuler;
+import base.Print;
+
+public class PE002 extends Print implements IProjectEuler {
+    @Override
+    public void Run() {
+        int fibLimit = 4000000;
+        int firstFib = 0;
+        int secondFib = 1;
+        int sumEvenValFib = 0;
+
+        while (secondFib < fibLimit) {
+            int tempSecondFib = secondFib;
+            secondFib += firstFib;
+            firstFib = tempSecondFib;
+            if (secondFib % 2 == 0)
+                sumEvenValFib += secondFib;
+        }
+
+        Println(sumEvenValFib);
+    }
+}
--- ProjectEulerApplication.java
+++ ProjectEulerApplication.java
@@ -1,10 +1,11 @@
 import ProjectEuler.PE001;
+import ProjectEuler.PE002;
 import base.IProjectEuler;
 
 public class ProjectEulerApplication {
 
     public  static  void main(String[] args){
-        IProjectEuler projectEuler = new PE001();
+        IProjectEuler projectEuler = new PE002();
         projectEuler.Run();
     }
 
