01e2e24bfdc9427e012ff650407808ddc541ff22
Emrecan Algül <can.emre1995@gmail.com>
2023-10-25 12:33:02 +0300

    Project Euler 018 soru çözümü.

--- ProjectEulerApplication.java
+++ ProjectEulerApplication.java
@@ -4,7 +4,7 @@ import base.IProjectEuler;
 public class ProjectEulerApplication {
 
     public  static  void main(String[] args){
-        IProjectEuler projectEuler = new PE017();
+        IProjectEuler projectEuler = new PE018();
         projectEuler.run();
     }
 
